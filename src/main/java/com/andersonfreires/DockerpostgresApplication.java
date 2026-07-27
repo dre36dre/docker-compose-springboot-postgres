179aa938e71bf43603a577a99441b5882acffdb2
Anderson freires <andersonfreires1979@gmail.com>
2026-07-27 16:49:56 -0300

    feat: add Docker Compose with Spring Boot and PostgresSQL

---
+++ src/main/java/com/andersonfreires/DockerpostgresApplication.java
@@ -0,0 +1,13 @@
+package com.andersonfreires;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerpostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerpostgresApplication.class, args);
+	}
+
+}
